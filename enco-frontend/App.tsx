1738684371d2af91e905ca868a4419e4ff83e5af
김내현 <rlasogus1@gmail.com>
2026-03-05 17:26:43 +0900

    refactor:modify NavigationContainer path

--- enco-frontend/App.tsx
+++ enco-frontend/App.tsx
@@ -1,7 +1,12 @@
 import './global.css';
 import RootNavigator from './src/navigation/RootNavigator';
+import { NavigationContainer } from '@react-navigation/native';
 function App() {
-  return <RootNavigator />;
+  return (
+    <NavigationContainer>
+      <RootNavigator />;
+    </NavigationContainer>
+  );
 }
 
 export default App;
