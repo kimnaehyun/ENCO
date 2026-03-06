9bbe81a0de3b695ca4afc444ffc30a59ef1a1223
김내현 <rlasogus1@gmail.com>
2026-03-06 10:44:05 +0900

    docs:accout screen을 gps screen으로 이름 변경

--- enco-frontend/src/screens/OnsitePayment/GPSScanScreen.tsx
+++ enco-frontend/src/screens/OnsitePayment/GPSScanScreen.tsx
@@ -1,9 +1,9 @@
 import { Text, View } from 'react-native';
 
-export default function AccountScreen() {
+export default function GPSScanScreen() {
   return (
     <View>
-      <Text>AccountScreen</Text>
+      <Text>GPSScanScreen</Text>
     </View>
   );
 }
