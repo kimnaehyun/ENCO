c2af1230c9f940ef3da8a20f1788f1e437449059
hyeyeok <juliago00@naver.com>
2026-03-09 15:42:27 +0900

    fix boximg

--- enco-shopping-mall/ut-hyeyeok/src/types/image.ts
+++ enco-shopping-mall/ut-hyeyeok/src/types/image.ts
@@ -1,6 +1,6 @@
 import accomoone from '../assets/image/accomoone.jpg'
 import accomotwo from '../assets/image/accomotwo.jpg'
-import boxjpg from '../assets/image/accomobox.jpg'
+import boxjpg from '../assets/image/box.jpg'
 export const images = {
     accomoone,boxjpg,accomotwo,
 }
