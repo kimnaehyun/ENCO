478e0054e5202ad8fad8eec11bfbae730a4cb98b
김내현 <rlasogus1@gmail.com>
2026-03-12 17:40:34 +0900

    feat:결제 성공 스크린 확인 버튼 생성

--- enco-frontend/src/screens/OnsitePayment/PaymentSuccess.tsx
+++ enco-frontend/src/screens/OnsitePayment/PaymentSuccess.tsx
@@ -1,8 +1,9 @@
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import React from 'react';
-import { useRoute } from '@react-navigation/native';
+import { CommonActions, useRoute } from '@react-navigation/native';
+import { ROUTES } from '../../constants/routes';
 
-export default function PaymentSuccess() {
+export default function PaymentSuccess({ navigation }: { navigation: any }) {
   const route = useRoute<any>();
 
   const { storeName, amount } = route.params;
@@ -12,6 +13,24 @@ export default function PaymentSuccess() {
       <Text>결제 성공</Text>
       <Text>매장: {storeName}</Text>
       <Text>금액: {amount}원</Text>
+      <Button
+        title="확인"
+        onPress={() => {
+          navigation.dispatch(
+            CommonActions.reset({
+              index: 0,
+              routes: [
+                {
+                  name: 'App',
+                  state: {
+                    routes: [{ name: ROUTES.TAB_HOME }],
+                  },
+                },
+              ],
+            }),
+          );
+        }}
+      />
     </View>
   );
 }
