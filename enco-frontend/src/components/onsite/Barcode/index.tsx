9fd4a0d776a7bf3665fd01b618bca99b9648beef
김내현 <rlasogus1@gmail.com>
2026-03-13 17:12:57 +0900

    refactor:모달 관련 코드 삭제

--- enco-frontend/src/components/onsite/Barcode/index.tsx
+++ enco-frontend/src/components/onsite/Barcode/index.tsx
@@ -1,26 +1,16 @@
-import { View, Modal, TouchableOpacity, Button, Pressable } from 'react-native';
-import React, { useState } from 'react';
-
+import { View } from 'react-native';
+import { useState } from 'react';
 import QR from './components/QR';
 import { Text } from 'react-native-gesture-handler';
 import CardRecommendation from './components/CardRecommendation';
 
 export default function index() {
   const [cardNumber, setCardNumber] = useState<number>(0);
-  const [modalVisible, setModalVisible] = useState(false);
-  const [modalType, setModalType] = useState<'barcode' | 'qr'>('barcode');
-
-  const openModal = (type: 'barcode' | 'qr') => {
-    setModalType(type);
-    setModalVisible(true);
-  };
 
   return (
     <View className="flex-1">
       <View className="flex-row w-full py-4 justify-around items-center">
-        <TouchableOpacity onPress={() => openModal('qr')}>
-          <QR cardNumber={cardNumber} className="w-24 h-24" />
-        </TouchableOpacity>
+        <QR cardNumber={cardNumber} className="w-24 h-24" />
       </View>
 
       <View className="flex-1">
@@ -31,23 +21,6 @@ export default function index() {
           <CardRecommendation onSelectCard={setCardNumber} />
         </View>
       </View>
-
-      <Modal
-        visible={modalVisible}
-        transparent
-        animationType="fade"
-        onRequestClose={() => setModalVisible(false)}
-      >
-        <View className="flex-1 justify-center items-center bg-white">
-          <View className={`bg-white flex items-end`}>
-            <Pressable onPress={() => setModalVisible(false)}>
-              <Text className="text-5xl">x</Text>
-            </Pressable>
-
-            <QR className="w-80 h-80" cardNumber={cardNumber} />
-          </View>
-        </View>
-      </Modal>
     </View>
   );
 }
