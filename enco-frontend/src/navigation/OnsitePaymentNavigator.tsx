6ada8539fa2f6b7ec0b48f191b9be183095829c4
김내현 <rlasogus1@gmail.com>
2026-03-13 17:06:54 +0900

    refactor:현장결제 플로우 수정

--- enco-frontend/src/navigation/OnsitePaymentNavigator.tsx
+++ enco-frontend/src/navigation/OnsitePaymentNavigator.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import PaymentMethodScreen from '../screens/OnsitePayment/PaymentMethodScreen';
-import GPSScanScreen from '../screens/OnsitePayment/GPSScanScreen';
 import PaymentSuccess from '../screens/OnsitePayment/PaymentSuccess';
-import PaymentPinScreen from '../screens/InternetPayment/PaymentPinScreen';
 
 export default function OnsitePaymentNavigator() {
   const Stack = createNativeStackNavigator();
@@ -14,11 +12,10 @@ export default function OnsitePaymentNavigator() {
       }}
     >
       <Stack.Screen
-        name="GPSScan"
-        component={GPSScanScreen}
+        name="PaymentMethod"
+        component={PaymentMethodScreen}
         options={{ headerShown: false }}
       />
-      <Stack.Screen name="PaymentMethod" component={PaymentMethodScreen} />
       <Stack.Screen
         name="PaymentSuccess"
         component={PaymentSuccess}
