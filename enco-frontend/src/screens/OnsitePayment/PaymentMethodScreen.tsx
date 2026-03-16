3b18381f2a3320adcf0f73935210efdec6219c44
김내현 <rlasogus1@gmail.com>
2026-03-16 11:36:41 +0900

    refactor:스타일 수정

--- enco-frontend/src/screens/OnsitePayment/PaymentMethodScreen.tsx
+++ enco-frontend/src/screens/OnsitePayment/PaymentMethodScreen.tsx
@@ -2,28 +2,23 @@ import { View } from 'react-native';
 import PaymentToggleButton from '../../components/onsite/PaymentToggleButton';
 import Barcode from '../../components/onsite/Barcode/';
 import QR from '../../components/onsite/QR';
-import { usePaymentStore } from '../../store/usePaymentStore';
+import { usePaymentMethod } from '../../hooks/usePaymentMethod';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export default function PaymentMethodScreen() {
-  const paymentMethodType = usePaymentStore(state => state.paymentMethod);
-
+  const { isBarcode } = usePaymentMethod();
   const insets = useSafeAreaInsets();
   return (
-    <View className="flex-1 bg-[#636363] p-4">
+    <View className="flex-1 bg-[#636363] px-6">
       <View
         style={{ paddingTop: insets.top }}
         className={
-          paymentMethodType === 'barcode'
-            ? ''
-            : 'absolute top-0 left-0 right-0 z-50 m-5'
+          isBarcode ? 'my-5' : 'absolute top-0 left-0 right-0 z-50 m-5'
         }
       >
         <PaymentToggleButton />
       </View>
-      <View className="flex-1">
-        {paymentMethodType === 'barcode' ? <Barcode /> : <QR />}
-      </View>
+      <View className="flex-1">{isBarcode ? <Barcode /> : <QR />}</View>
     </View>
   );
 }
