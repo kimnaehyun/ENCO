9c136b63a28555b2954b62c834b080a421589306
장하은 <haeun7700@hs.ac.kr>
2026-03-16 16:59:38 +0900

    git commit -m "Feat: 채팅 공통 에러 처리 추가" -m "채팅 공통 에러 처리 추가"

---
+++ enco-backend/chat/src/main/java/io/ssafy/chat/global/config/error/ResponseError.java
@@ -0,0 +1,18 @@
+package io.ssafy.chat.global.config.error;
+
+import lombok.Builder;
+
+@Builder
+public record ResponseError (
+    int httpCode,
+    String message,
+    String errorType
+){
+    public static ResponseError of(ErrorCode e) {
+        return ResponseError.builder()
+                .httpCode(e.getHttpStatusCode().value())
+                .message(e.getErrorMessage())
+                .errorType(e.getErrorName())
+                .build();
+    }
+}
