2d03135658c87fa9d9f7dd174e3cbb72b4d11f3d
김내현 <rlasogus1@gmail.com>
2026-03-16 17:40:34 +0900

    feat:뒤로가기 사진 추가

--- enco-frontend/src/types/images.ts
+++ enco-frontend/src/types/images.ts
@@ -13,6 +13,7 @@ export const images = {
   paymentIcon: require('../assets/icons/payment.png'),
   voteIcon: require('../assets/icons/vote.png'),
   internetPaymentHamco: require('../assets/icons/internet_payment_hamco.png'),
+  left_arrow: require('../assets/icons/left_arrow.png'),
   qr: [
     require('../assets/images/qr0.png'),
     require('../assets/images/qr1.png'),
