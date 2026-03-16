001f268fefab9d0b364bc634e9ddf81093c3b621
장하은 <haeun7700@hs.ac.kr>
2026-03-16 16:59:52 +0900

    git commit -m "Feat: 결제 공통 에러 처리 추가" -m "결제 공통 에러 처리 추가"

---
+++ enco-backend/payment/src/main/java/io/ssafy/payment/global/config/error/ResponseError.java
@@ -0,0 +1,18 @@
+package io.ssafy.payment.global.config.error;
+
+import lombok.Builder;
+
+@Builder
+public record ResponseError (
+    int httpCode,
+    String message,
+    String errorType
+){
+    public static ResponseError of(ErrorCode e) {
+        return ResponseError.builder()
+                .httpCode(e.getHttpStatusCode().value())
+                .message(e.getErrorMessage())
+                .errorType(e.getErrorName())
+                .build();
+    }
+}
