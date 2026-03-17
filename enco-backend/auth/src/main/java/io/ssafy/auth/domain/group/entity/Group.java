b9adb739e6c43b39bd8004276a603bb6a2c1af85
장하은 <haeun7700@hs.ac.kr>
2026-03-17 09:31:10 +0900

    Fix: 그룹 엔티티명 예약어 충돌 해결 및 레포지토리명 수정

--- enco-backend/auth/src/main/java/io/ssafy/auth/domain/group/entity/Group.java
+++ enco-backend/auth/src/main/java/io/ssafy/auth/domain/group/entity/Group.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name = "groups")
+@Table(name = "`groups`")
 @EntityListeners(AuditingEntityListener.class)
 @Builder
 @AllArgsConstructor
--- enco-backend/auth/src/main/java/io/ssafy/auth/domain/group/repository/GroupTypeRepository.java
+++ enco-backend/auth/src/main/java/io/ssafy/auth/domain/group/repository/GroupTypeRepository.java
@@ -1,8 +1,9 @@
 package io.ssafy.auth.domain.group.repository;
 
+import io.ssafy.auth.domain.group.entity.GroupType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Type extends JpaRepository<Type, Long> {
+public interface GroupTypeRepository extends JpaRepository<GroupType, Long> {
 }
