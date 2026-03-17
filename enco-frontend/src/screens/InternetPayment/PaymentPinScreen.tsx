2a43c66f3530f74fcdae3198069ce980a5972d38
김내현 <rlasogus1@gmail.com>
2026-03-17 11:20:02 +0900

    feat:투표 생성 페이지로 이동 로직 추가

--- enco-frontend/src/screens/InternetPayment/PaymentPinScreen.tsx
+++ enco-frontend/src/screens/InternetPayment/PaymentPinScreen.tsx
@@ -1,16 +1,19 @@
 import { View, Alert } from 'react-native';
 import React, { useState } from 'react';
 import PinEntry from '../../components/pin/PinEntry';
+import { useNavigation } from '@react-navigation/native';
 
 const TEST_PIN = '2580';
 
 export default function PaymentPinScreen() {
   const [resetKey, setResetKey] = useState(0);
+  const navigation = useNavigation<any>();
 
   const handlePinComplete = (pin: string) => {
     const isValid = pin === TEST_PIN;
 
     if (isValid) {
+      navigation.navigate('VoteCreateScreen');
     }
 
     Alert.alert('인증 실패', 'pin 번호가 올바르지 않습니다.');
