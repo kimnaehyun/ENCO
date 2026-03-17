2bdc4416a2e3c2f9ed9c22f7da2a2317487a0f10
김내현 <rlasogus1@gmail.com>
2026-03-17 10:14:37 +0900

    refactor:파일 이름 변경

--- enco-frontend/src/components/common/KeyValueRow.tsx
+++ enco-frontend/src/components/common/KeyValueRow.tsx
@@ -1,7 +1,7 @@
 import { View, Text } from 'react-native';
 import React from 'react';
 
-export default function PaymentHistory({
+export default function KeyValueRow({
   title,
   children,
 }: {
