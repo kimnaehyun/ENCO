de0cfc209545c6f56b6248709b178b56baaf0994
김내현 <rlasogus1@gmail.com>
2026-03-17 10:47:30 +0900

    feat:인터넷 결제 카드 선택 페이지

--- enco-frontend/src/screens/InternetPayment/CardChoiceScreen.tsx
+++ enco-frontend/src/screens/InternetPayment/CardChoiceScreen.tsx
@@ -4,9 +4,12 @@ import { useNavigation, useRoute } from '@react-navigation/native';
 import { images } from '../../types/images';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import CardRecommendation from '../../components/onsite/Barcode/components/CardRecommendation';
+import KeyValueRow from '../../components/common/KeyValueRow';
+import PointToggleButton from '../../components/payment/PointToggleButton';
+import PayButton from '../../components/internet/PayButton';
 
 export default function CardChoiceScreen() {
-  const navigation = useNavigation();
+  const navigation = useNavigation<any>();
   const route = useRoute();
   const params = route.params as { title: string };
   const insets = useSafeAreaInsets();
@@ -23,10 +26,31 @@ export default function CardChoiceScreen() {
         <View>
           <Text className="text-xl">결제 카드 선택</Text>
         </View>
-        <View className="flex-1 border border-solid border-black">
-          <CardRecommendation onSelectCard={setCardNumber} />
+        <View className="flex-1">
+          <View className="h-96">
+            <CardRecommendation onSelectCard={setCardNumber} />
+          </View>
+          <View className="flex-1 gap-5">
+            <KeyValueRow title="금액">
+              <Text className="font-bold text-[20px]">789,000</Text>
+            </KeyValueRow>
+            <View className="flex-row justify-between items-center ">
+              <View className="flex-row items-center">
+                <Text className="text-[20px]">보유 포인트</Text>
+                <Text className="text-[#1428A0] text-[20px]">80P</Text>
+              </View>
+              <PointToggleButton />
+            </View>
+            <KeyValueRow title="가맹점명">
+              <Text className="font-bold text-[20px]">여기 엇-혜역</Text>
+            </KeyValueRow>
+          </View>
+          <View className="flex items-center">
+            <PayButton
+              onPress={() => navigation.navigate('PaymentPinScreen')}
+            />
+          </View>
         </View>
-        <View></View>
       </View>
     </View>
   );
