42768526dad3a697cb58eaf56d8f014100a060c3
hyeyeok <juliago00@naver.com>
2026-03-19 10:43:23 +0900

    fix conflict

--- enco-backend/chat/src/main/java/io/ssafy/chat/config/WebSocketConfig.java
+++ enco-backend/chat/src/main/java/io/ssafy/chat/config/WebSocketConfig.java
@@ -34,7 +34,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-stomp")
-                .setAllowedOriginPatterns("*")  // CORS 허용 (개발용)
-                .withSockJS();                   // SockJS fallback 지원
+                .setAllowedOriginPatterns("*"); // CORS 허용 (개발용)
+
     }
 }
