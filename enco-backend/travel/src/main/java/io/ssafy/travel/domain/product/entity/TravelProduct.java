eb18d191caef3f31cb353c79124da6c3ee664983
장하은 <haeun7700@hs.ac.kr>
2026-03-21 20:12:04 +0900

    Feat : 테이블명 수정

--- enco-backend/travel/src/main/java/io/ssafy/travel/domain/product/entity/TravelProduct.java
+++ enco-backend/travel/src/main/java/io/ssafy/travel/domain/product/entity/TravelProduct.java
@@ -12,7 +12,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-@Entity(name = "travel_product")
+@Entity(name = "travel_products")
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
