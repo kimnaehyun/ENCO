92e95bd499f89f8ed2b9e544e1ccccf75ba01dc4
장하은 <haeun7700@hs.ac.kr>
2026-03-22 19:53:05 +0900

    Feat : controller 이름 변경

--- enco-backend/payment/src/main/java/io/ssafy/payment/domain/account/controller/AccountController.java
+++ enco-backend/payment/src/main/java/io/ssafy/payment/domain/account/controller/AccountController.java
@@ -1,47 +1,29 @@
 package io.ssafy.payment.domain.account.controller;
 
 import io.ssafy.payment.domain.account.dto.request.CardIssueRequestDto;
-import io.ssafy.payment.domain.account.dto.request.PaymentCreateRequestDto;
 import io.ssafy.payment.domain.account.dto.response.CardIssueResponseDto;
-import io.ssafy.payment.domain.account.dto.response.GroupAccountCardResponseDto;
-import io.ssafy.payment.domain.account.dto.response.PaymentCreateResponseDto;
 import io.ssafy.payment.domain.account.service.AccountService;
-import io.ssafy.payment.domain.card.dto.response.GroupCardResponseDto;
-import io.ssafy.payment.domain.card.service.CardService;
 import io.ssafy.payment.global.common.response.CommonResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/internal")
+@RequestMapping("/api/v1/accounts")
 @RequiredArgsConstructor
 public class AccountController {
 
     private final AccountService accountService;
-    private final CardService cardService;
 
-    @PostMapping("/payments/graoup-account")
-    public ResponseEntity<CommonResponse<PaymentCreateResponseDto>> createAccountAndCard(@RequestBody PaymentCreateRequestDto request) {
-        PaymentCreateResponseDto response = accountService.createAccountAndCard(request);
-        return ResponseEntity.status(HttpStatus.CREATED).body(CommonResponse.success(response));
-    }
-
-    @GetMapping("/payments/groups/{groupId}/card")
-    public ResponseEntity<CommonResponse<GroupCardResponseDto>> getGroupCard(@PathVariable Long groupId) {
-        return ResponseEntity.ok(CommonResponse.success(cardService.getGroupCard(groupId)));
-    }
-
-    @GetMapping("/accounts/by-ids")
-    public ResponseEntity<CommonResponse<List<GroupAccountCardResponseDto>>> getAccountsByIds(
-            @RequestParam List<Long> accountIds) {
-        return ResponseEntity.ok(CommonResponse.success(accountService.getAccountsByIds(accountIds)));
-    }
-
-    @PostMapping("/accounts/cards")
+    /**
+     * 계좌 카드 추가 발급
+     * @param request
+     * @return
+     */
+    @PostMapping("/card-add")
     public ResponseEntity<CommonResponse<CardIssueResponseDto>> issueAdditionalCard(
             @RequestBody CardIssueRequestDto request) {
         return ResponseEntity.ok(CommonResponse.success(accountService.issueAdditionalCard(request)));
