aeea03a3794360afcc64df1a35c86ca7c36db024
고혜역 <juliago00@naver.com>
2026-03-22 14:52:13 +0900

    Edit config.ts

--- enco-shopping-mall/ut-hyeyeok/src/api/config.ts
+++ enco-shopping-mall/ut-hyeyeok/src/api/config.ts
@@ -1,1 +1,1 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://api.ssafywte.site/payment-service';
+export const API_BASE_URL = 'https://api.ssafywte.site/travel-service';
