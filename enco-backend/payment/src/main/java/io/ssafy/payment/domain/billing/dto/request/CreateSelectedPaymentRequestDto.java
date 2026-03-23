801ae4c24009473b17aa735121cc76f3251bd554
정희수 <huissppea@gmail.com>
2026-03-23 11:50:37 +0900

    Feat: 테스트 및 은행 코드 추가
    - 은행 코드 컬럼 추가
    - RequestBody 의 멱등키 삭제
    - 은행 코드 매칭하는 메서드 추가

--- enco-backend/payment/src/main/java/io/ssafy/payment/domain/billing/dto/request/CreateSelectedPaymentRequestDto.java
+++ enco-backend/payment/src/main/java/io/ssafy/payment/domain/billing/dto/request/CreateSelectedPaymentRequestDto.java
@@ -8,6 +8,9 @@ import java.util.ArrayList;
 
 public record CreateSelectedPaymentRequestDto(
 
+        String withdrawAccountBankName,
+        String withdrawAccountNumber,
+
         @NotNull(message = "입금 금액은 필수입니다.")
         @DecimalMin(value = "0.01", message = "입금 금액은 0보다 커야 합니다.")
         BigDecimal amount,
