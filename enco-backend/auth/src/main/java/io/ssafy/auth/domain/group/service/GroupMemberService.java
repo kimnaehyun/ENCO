fcc6f0598d1d380a3e8a79145f2e5438826b902b
정희수 <huissppea@gmail.com>
2026-03-23 09:50:58 +0900

    Feat: 로그 추가

--- enco-backend/auth/src/main/java/io/ssafy/auth/domain/group/service/GroupMemberService.java
+++ enco-backend/auth/src/main/java/io/ssafy/auth/domain/group/service/GroupMemberService.java
@@ -10,11 +10,13 @@ import io.ssafy.auth.domain.group.repository.GroupUserRepository;
 import io.ssafy.auth.global.common.error.CustomException;
 import io.ssafy.auth.global.common.error.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class GroupMemberService {
@@ -36,8 +38,10 @@ public class GroupMemberService {
 
     @Transactional
     public void updateMemberRole(Long groupId, Long requesterId, Long targetUserId, UpdateMemberRoleRequestDto request) {
+        log.info("groupId={}, requesterId={}, targetUserId={}", groupId, requesterId, targetUserId);
         GroupUser requester = groupUserRepository.findByGroup_IdAndUser_IdAndIsDeletedFalse(groupId, requesterId)
                 .orElseThrow(() -> new CustomException(ErrorCode.GROUP_MEMBER_NOT_FOUND));
+        log.info("요청자 확인 완료");
 
         if (requester.getRole() != Role.LEADER) {
             throw new CustomException(ErrorCode.FORBIDDEN);
@@ -46,5 +50,6 @@ public class GroupMemberService {
         groupUserRepository.findByGroup_IdAndUser_IdAndIsDeletedFalse(groupId, targetUserId)
                 .orElseThrow(() -> new CustomException(ErrorCode.GROUP_MEMBER_NOT_FOUND))
                 .updateRole(request.role());
+        log.info("타겟 확인 완료");
     }
 }
