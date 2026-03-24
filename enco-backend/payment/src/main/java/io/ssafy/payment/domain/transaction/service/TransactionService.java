ad849bb5d408504bba8fd839445d70d579c78295
정희수 <huissppea@gmail.com>
2026-03-24 14:58:52 +0900

    Refactor: 예외 처리 변경

--- enco-backend/payment/src/main/java/io/ssafy/payment/domain/transaction/service/TransactionService.java
+++ enco-backend/payment/src/main/java/io/ssafy/payment/domain/transaction/service/TransactionService.java
@@ -55,7 +55,7 @@ public class TransactionService {
             int size
     ) {
         Account account = accountRepository.findByGroupIdAndIsDeletedFalse(groupId)
-                .orElseThrow(() -> new CustomException(ErrorCode.SERVER_ERROR));
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_ACCOUNT));
 
         LocalDateTime cursor = cursorEpochMilli != null
                 ? LocalDateTime.ofInstant(Instant.ofEpochMilli(cursorEpochMilli), ZoneOffset.UTC)
@@ -106,13 +106,13 @@ public class TransactionService {
     @Transactional(readOnly = true)
     public TransactionDetailResponseDto getTransactionDetail(Long groupId, Long transactionId) {
         Account account = accountRepository.findByGroupIdAndIsDeletedFalse(groupId)
-                .orElseThrow(() -> new CustomException(ErrorCode.SERVER_ERROR));
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_ACCOUNT));
 
         TransactionHistory th = transactionHistoryRepository.findById(transactionId)
                 .orElseThrow(() -> new CustomException(ErrorCode.TRANSACTION_NOT_FOUND));
 
         if (!th.getAccountId().equals(account.getId())) {
-            throw new CustomException(ErrorCode.TRANSACTION_NOT_FOUND);
+            throw new CustomException(ErrorCode.TRANSACTION_ACCESS_DENIED);
         }
 
         String cardName = null;
@@ -130,13 +130,13 @@ public class TransactionService {
     @Transactional
     public String attachReceiptContent(Long groupId, Long transactionId, MultipartFile file, PaymentInfoDto data) {
         Account account = accountRepository.findByGroupIdAndIsDeletedFalse(groupId)
-                .orElseThrow(() -> new CustomException(ErrorCode.SERVER_ERROR));
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_ACCOUNT));
 
         TransactionHistory th = transactionHistoryRepository.findById(transactionId)
                 .orElseThrow(() -> new CustomException(ErrorCode.TRANSACTION_NOT_FOUND));
 
         if (!th.getAccountId().equals(account.getId())) {
-            throw new CustomException(ErrorCode.TRANSACTION_NOT_FOUND);
+            throw new CustomException(ErrorCode.TRANSACTION_ACCESS_DENIED);
         }
 
         // 영수증 이미지 업로드
