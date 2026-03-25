da8593833970062a792a0b51d12bc5556ba57144
김윤성 <a058594@gmail.com>
2026-03-25 09:07:01 +0900

    Chore: Firebase EC2서버 환경에 맞게 수정

--- enco-backend/chat/src/main/java/io/ssafy/chat/config/FcmConfig.java
+++ enco-backend/chat/src/main/java/io/ssafy/chat/config/FcmConfig.java
@@ -6,9 +6,9 @@ import com.google.firebase.FirebaseOptions;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
 import jakarta.annotation.PostConstruct;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -22,12 +22,13 @@ public class FcmConfig {
     @PostConstruct
     public void init() throws IOException {
         if (FirebaseApp.getApps().isEmpty()) {
-            InputStream serviceAccount = new ClassPathResource(serviceAccountPath).getInputStream();
-            FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .build();
-            FirebaseApp.initializeApp(options);
-            log.info("Firebase 초기화 완료");
+            try (InputStream serviceAccount = new FileInputStream(serviceAccountPath)) {
+                FirebaseOptions options = FirebaseOptions.builder()
+                        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                        .build();
+                FirebaseApp.initializeApp(options);
+                log.info("Firebase 초기화 완료");
+            }
         }
     }
 }
