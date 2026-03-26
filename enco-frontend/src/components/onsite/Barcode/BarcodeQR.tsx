06964b3d0d3f274a1736c004328d92e238e411ba
김내현 <rlasogus1@gmail.com>
2026-03-26 16:19:40 +0900

    refactor:qr 사진에서 qr 생성으로 변경

--- enco-frontend/src/components/onsite/Barcode/BarcodeQR.tsx
+++ enco-frontend/src/components/onsite/Barcode/BarcodeQR.tsx
@@ -1,19 +1,14 @@
-import { Image } from 'react-native';
 import React from 'react';
-import { images } from '@/types/images';
 
-export default function BarcodeQR({
-  cardNumber,
-  className,
-}: {
-  cardNumber: number;
-  className: string;
-}) {
-  return (
-    <Image
-      className={className}
-      source={images.qr[cardNumber]}
-      resizeMode="contain"
-    />
-  );
+import QRCode from 'react-native-qrcode-svg';
+
+export default function BarcodeQR({ cardId }: { cardId: number }) {
+  const data = {
+    cardId,
+    userId: 123,
+    name: '홍길동',
+    amount: 5000,
+  };
+  console.log('QR cardId:', cardId);
+  return <QRCode key={cardId} value={JSON.stringify(data)} size={300} />;
 }
