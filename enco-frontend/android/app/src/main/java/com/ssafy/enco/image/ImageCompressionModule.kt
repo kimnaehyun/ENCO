d644917c4ae0f746a6cd5d9817cc72744d6e2356
김내현 <rlasogus1@gmail.com>
2026-03-26 10:37:40 +0900

    refactor:패키지명 변경

--- enco-frontend/android/app/src/main/java/com/ssafy/enco/image/ImageCompressionModule.kt
+++ enco-frontend/android/app/src/main/java/com/ssafy/enco/image/ImageCompressionModule.kt
@@ -1,4 +1,4 @@
-package com.frontend.image
+package com.ssafy.enco.image
 
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
--- enco-frontend/android/app/src/main/java/com/ssafy/enco/image/ImageCompressionPackage.kt
+++ enco-frontend/android/app/src/main/java/com/ssafy/enco/image/ImageCompressionPackage.kt
@@ -1,4 +1,4 @@
-package com.frontend.image
+package com.ssafy.enco.image
 
 import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
