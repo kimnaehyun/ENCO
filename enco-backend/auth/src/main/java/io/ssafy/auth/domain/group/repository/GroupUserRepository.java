473c41a5b286a7656008d058616079a6e2f0e228
장하은 <haeun7700@hs.ac.kr>
2026-03-27 08:14:49 +0900

    Fix :주석 삭제

--- enco-backend/auth/src/main/java/io/ssafy/auth/domain/group/repository/GroupUserRepository.java
+++ enco-backend/auth/src/main/java/io/ssafy/auth/domain/group/repository/GroupUserRepository.java
@@ -5,7 +5,6 @@ import io.ssafy.auth.domain.group.entity.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
--- enco-backend/payment/src/main/java/io/ssafy/payment/domain/onsite/service/OnsitePaymentService.java
+++ enco-backend/payment/src/main/java/io/ssafy/payment/domain/onsite/service/OnsitePaymentService.java
@@ -19,8 +19,6 @@ import org.springframework.stereotype.Service;
 
 import java.time.Duration;
 import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 
 @Slf4j
