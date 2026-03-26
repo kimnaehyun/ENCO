4456e99fe12f1b8019b98621dcc87e48ce60f3b5
장하은 <haeun7700@hs.ac.kr>
2026-03-26 09:35:57 +0900

    Feat : 현장 결제 알림 경로 수정

--- enco-backend/auth/src/main/java/io/ssafy/auth/domain/group/repository/GroupUserRepository.java
+++ enco-backend/auth/src/main/java/io/ssafy/auth/domain/group/repository/GroupUserRepository.java
@@ -5,6 +5,7 @@ import io.ssafy.auth.domain.group.entity.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,4 +22,6 @@ public interface GroupUserRepository extends JpaRepository<GroupUser, Long> {
     int countByGroup_IdAndIsDeletedFalse(Long groupId);
 
     int countByGroupId(Long groupId);
+
+    List<GroupUser> findByGroup_IdAndIsDeletedFalse(Long groupId);
 }
