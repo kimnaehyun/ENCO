440a0c290ba9dd3548841cb6a2d74e21ea1e0ffc
hyeyeok <juliago00@naver.com>
2026-03-27 14:55:42 +0900

    Fix : 버그 수정

--- enco-frontend/src/components/groupChat/ChatMessageList.tsx
+++ enco-frontend/src/components/groupChat/ChatMessageList.tsx
@@ -1,4 +1,4 @@
-npm run androidimport { FlatList, ListRenderItem } from 'react-native';
+import { FlatList, ListRenderItem } from 'react-native';
 import { ChatItem, ChatMessageListProps } from '@/types/chat';
 import ChatMessage from '@/components/groupChat/ChatMessage';
 import Chatbot from '@/components/groupChat/Chatbot';
