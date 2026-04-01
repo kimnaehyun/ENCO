3cac067d2b68eced64e0d44d6356a7dbc85e7d9d
김내현 <rlasogus1@gmail.com>
2026-04-01 09:34:06 +0900

    refactor:PaymentStartScreen 파일 any 타입 변경

--- enco-frontend/src/screens/payment/PaymentStartScreen.tsx
+++ enco-frontend/src/screens/payment/PaymentStartScreen.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Dimensions, Image, StyleSheet, View } from 'react-native';
-import { useNavigation, useRoute } from '@react-navigation/native';
+import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
 import ScreenLayout from '../../components/ScreenLayout';
 import { images } from '../../types/images';
 import PayButton from '../../components/internet/PayButton';
+import { InternetPayStackParamList } from '@/types/navigation';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
 const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get('window');
 
@@ -13,15 +15,19 @@ const IMG_SIZE = Math.min(SCREEN_WIDTH * 0.6, SCREEN_HEIGHT * 0.32);
 // 카드 최소 높이: 이미지 + 버튼 + 여백 기준
 const CARD_MIN_HEIGHT = Math.min(IMG_SIZE + 120, SCREEN_HEIGHT * 0.75);
 
+type PaymentStartRouteProp = RouteProp<
+  InternetPayStackParamList,
+  'PaymentStartScreen'
+>;
+type PaymentStartNavigationProp = NativeStackNavigationProp<
+  InternetPayStackParamList,
+  'PaymentStartScreen'
+>;
+
 export default function PaymentStartScreen() {
-  const navigation = useNavigation<any>();
-  const route = useRoute();
-  const { amount, storeName, callbackUrl, orderId } = (route.params ?? {}) as {
-    amount?: number;
-    storeName?: string;
-    callbackUrl?: string;
-    orderId: string;
-  };
+  const navigation = useNavigation<PaymentStartNavigationProp>();
+  const route = useRoute<PaymentStartRouteProp>();
+  const { amount, storeName, callbackUrl, orderId } = route.params ?? {};
 
   return (
     <ScreenLayout>
